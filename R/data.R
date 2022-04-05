2649bbd5aaaa9ef7955ec0426e430f234d26538a
Victor Navarro <navarrov@cardiff.ac.uk>
2022-04-05 23:44:33 +0100

    update to pati data and fitting_heidi vignette

--- R/data.R
+++ R/data.R
@@ -5,10 +5,11 @@
 #' @format A tibble with the following variables:
 #' \describe{
 #'   \item{subject}{subject identifier}
-#'   \item{rpert}{responses per trial}
-#'   \item{lever}{lever presented on the trial: L = left; R = right}
-#'   \item{response}{the response: lp = lever press; np = nose poke}
 #'   \item{block}{the 2-session block of training (1 to 8)}
+#'   \item{lever}{lever presented on the trial: L = left; R = right}
+#'   \item{us}{the stimulus that followed the lever: P = pellet; S = sucrose}
+#'   \item{response}{the response: lp = lever press; np = nose poke}
+#'   \item{rpert}{responses per trial}
 #'   ...
 #' }
 #' @source {Patittucci et al. (2016). JEP:ALC}
