a0e76834c6b575c218a070e89393b8338471a4ee
Victor Navarro <navarrov@cardiff.ac.uk>
2024-02-15 09:30:58 +0000

    versioning

--- R/model_class.R
+++ R/model_class.R
@@ -16,6 +16,14 @@ setClass(
   representation(
     model = "character",
     model_fn = "function",
-    state = "list"
+    state = "list",
+    parameters = "list"
   )
 )
+
+# Note: This class will have its own "forward" method that takes
+# a mapping and experience parameter and returns result.
+# That way one can save the state of the model and retrain it. In fact,
+# that might be a more elegant way of dealing with phases.
+# Given that the "forward" method will return a list, the current
+# implementation does not conflict with future changes.
